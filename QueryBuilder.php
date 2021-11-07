053a4f28f3ce40f5d35668004b32fced5009654f
markelovdn <markelovdn@gmail.com>
2021-11-07 11:36:00 +0300

    qbFinaldebug

--- QueryBuilder.php
+++ QueryBuilder.php
@@ -11,6 +11,11 @@ class QueryBuilder
         $this->qs = "";
     }
 
+    /**
+     * @param $table string
+     * @param $data array
+     * insert $data in $table data base
+     */
     public function create($table, $data) {
         $keys = implode(', ', array_keys($data));
         $tags = ':'. implode(', :', array_keys($data));
@@ -20,6 +25,12 @@ class QueryBuilder
         $statement->execute($data);
     }
 
+    /**
+     * @param $table string
+     * @param $data array
+     * @param $id string
+     * update $table $data where id=$id
+     */
     public function update($table, $data, $id) {
         $keys = array_keys($data);
 
@@ -38,6 +49,11 @@ class QueryBuilder
         $statement->execute($data);
     }
 
+    /**
+     * @param $table string
+     * @param $id string
+     * delete row where id=$id in $table data base
+     */
     public function delete($table, $id) {
         $sql = "DELETE FROM post WHERE id=:id";
 
@@ -47,18 +63,35 @@ class QueryBuilder
         ]);
     }
 
+    /**
+     * @param $table string
+     * @return $this string
+     * return select all rows from $table
+     */
     public function selectAll($table)
     {
        $this->qs .= "SELECT * FROM {$table}".' ';
        return $this;
     }
 
+    /**
+     * @param array $column
+     * @param $table string
+     * @return $this string
+     * return select $column from $table
+     */
     public function select($column = [], $table)
     {
         $this->qs .= "SELECT $column FROM {$table}".' ';
         return $this;
     }
 
+    /**
+     * @param $column string
+     * @param $sign string
+     * @param null $condition string
+     * @return $this string
+     */
     public function where($column, $sign, $condition=null)
     {
         if (!$condition) {
@@ -67,6 +100,12 @@ class QueryBuilder
         return $this;
     }
 
+    /**
+     * @param $column string
+     * @param int $direction
+     * @return $this string
+     *
+     */
     public function orderBy($column, $direction = 0)
     {
         if (!$direction) {
@@ -75,15 +114,35 @@ class QueryBuilder
         return $this;
     }
 
+    /**
+     * @param $column string
+     * @return $this string
+     */
     public function groupBy($column)
     {
         $this->qs .= "GROUP BY {$column}".' '   ;
         return $this;
     }
 
-    public function between($min, $max)
+    /**
+     * @param $min string (date formate yyyy-mm-dd)
+     * @param $max string (date formate yyyy-mm-dd)
+     * @return $this string
+     */
+    public function betweendate($mindate, $maxdate)
     {
-        $this->qs .= "BETWEEN CAST('{$min}' AS DATE) AND CAST('{$max}' AS DATE);".' '   ;
+        $this->qs .= "BETWEEN CAST('{$mindate}' AS DATE) AND CAST('{$maxdate}' AS DATE)".' ';
+        return $this;
+    }
+
+    /**
+     * @param $minvalue string, int
+     * @param $maxvalue string, int
+     * @return $this
+     */
+    public function between($minvalue, $maxvalue)
+    {
+        $this->qs .= "BETWEEN {$minvalue} AND {$maxvalue}".' ';
         return $this;
     }
 
@@ -159,20 +218,3 @@ class QueryBuilder
         die;
            }
 }
-
-//    public function getOne($table, $id) {
-//    $sql = "SELECT * FROM post WHERE id=:id";
-//
-//    $statement = $this->pdo->prepare($sql);
-//    $statement->bindValue(':id', $id);
-//    $statement->execute();
-//    $result = $statement->fetch(PDO::FETCH_ASSOC);
-//    return $result;
-//}
-
-//    public function getAll ($table) {
-//        $sql = "SELECT * FROM {$table}";
-//        $statement = $this->pdo->prepare($sql);
-//        $statement->execute();
-//        return $statement->fetchAll(PDO::FETCH_ASSOC);
-//    }
